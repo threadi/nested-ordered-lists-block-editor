d60195f375543b186ca152b62d128373909adf10
Thomas Zwirner <tzwirne@gmx.de>
2024-12-26 13:17:31 +0100

    add style minification

--- nested-ordered-lists-for-block-editor.php
+++ nested-ordered-lists-for-block-editor.php
@@ -14,17 +14,29 @@
  * @package nested-ordered-lists-for-block-editor
  */
 
+// set version.
+const NOLG_VERSION = '@@VersionNumber@@';
+
 /**
  * Enqueue frontend style.
  *
  * @return void
  */
 function nolg_frontend_style(): void {
+	// get the file name for the style file.
+	$css_file = 'css/style.css';
+
+	// if debug-mode is not enabled, use minified file.
+	if ( ! defined( 'WP_DEBUG' ) || ( defined( 'WP_DEBUG' ) && ! WP_DEBUG ) ) {
+		$css_file = str_replace( '.css', '.min.css', $css_file );
+	}
+
+	// enqueue the file.
 	wp_enqueue_style(
-		'nolg-list-css',
-		plugins_url( 'css/style.css', __FILE__ ),
+		'nolg-list',
+		plugins_url( $css_file, __FILE__ ),
 		array(),
-		filemtime( plugin_dir_path( __FILE__ ) . 'css/style.css' )
+		nolg_get_file_version( plugin_dir_path( __FILE__ ) . $css_file )
 	);
 }
 add_action( 'wp_enqueue_scripts', 'nolg_frontend_style' );
@@ -42,7 +54,7 @@ function nolg_assets(): void {
 			'nolg-backend',
 			plugins_url( 'attributes/listOption.js', __FILE__ ),
 			array( 'wp-blocks', 'wp-element', 'wp-components', 'wp-i18n', 'wp-block-editor' ),
-			filemtime( plugin_dir_path( __FILE__ ) . 'attributes/listOption.js' ),
+			nolg_get_file_version( plugin_dir_path( __FILE__ ) . 'attributes/listOption.js' ),
 			true
 		);
 
@@ -51,7 +63,7 @@ function nolg_assets(): void {
 			'nolg-admin',
 			plugins_url( 'admin/style.css', __FILE__ ),
 			array(),
-			filemtime( plugin_dir_path( __FILE__ ) . 'admin/style.css' )
+			nolg_get_file_version( plugin_dir_path( __FILE__ ) . 'admin/style.css' )
 		);
 	}
 
@@ -67,3 +79,32 @@ function nolg_assets(): void {
 	}
 }
 add_action( 'enqueue_block_assets', 'nolg_assets' );
+
+/**
+ * Return the version of the given file.
+ *
+ * With WP_DEBUG or plugin-debug enabled its @filemtime().
+ * Without this it's the plugin-version.
+ *
+ * @param string $filepath The absolute path to the requested file.
+ *
+ * @return string
+ */
+function nolg_get_file_version( string $filepath ): string {
+	// check for WP_DEBUG.
+	if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
+		return filemtime( $filepath );
+	}
+
+	$plugin_version = NOLG_VERSION;
+
+	/**
+	 * Filter the used file version (for JS- and CSS-files which get enqueued).
+	 *
+	 * @since 2.0.0 Available since 2.0.0.
+	 *
+	 * @param string $plugin_version The plugin-version.
+	 * @param string $filepath The absolute path to the requested file.
+	 */
+	return apply_filters( 'nolg_file_version', $plugin_version, $filepath );
+}
